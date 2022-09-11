03bb0880b39a51791a6a90749ab44fa3355281fb
Daniel <dannyb829@gmail.com>
2022-09-10 23:00:07 -0400

    user update null with hash username fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
     has_many :messages, dependent: :destroy
     has_many :notifications, dependent: :destroy
 
-    validates :username, presence: true, uniqueness: true, length: { minimum: 5}
+    validates :username, presence: true, uniqueness: true, length: { minimum: 5},
+    unless: proc { |z| z.username.blank? }
     validates :password, presence: true, on: :update, length: { minimum: 6 }, 
     unless: proc { |x| x.password.blank? }
 end
